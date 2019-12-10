c6e054b158df6fc92bd52f0cbebb592d7fa4e55e
MaZaJoM <jmartinzaballos@yahoo.com>
2019-12-10 23:44:56 +0100

    creacion de tablas, mensajes, categorias,sub_categorias y servicios

---
+++ database/migrations/2019_12_10_222427_create_mensajes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMensajesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mensajes', function (Blueprint $table) {
+            $table->bigIncrements('id_mensaje');
+            $table->string('emisor');
+            $table->string('receptor');
+            $table->string('contenido');
+            $table->string('tipo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mensajes');
+    }
+}
