66e7ee023a71bbb2dbce48e1cbe118f7c8b1700f
MaZaJoM <jmartinzaballos@yahoo.com>
2019-12-12 13:24:01 +0100

    para probar codepen

--- resources/views/multimedia.blade.php
+++ resources/views/multimedia.blade.php
@@ -11,6 +11,9 @@
     <link rel="ICON" type="iMAGEN/PNG" href="{{ asset('img/pestaña.png')}}"/>
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.min.css')}}">
+    
+    <!-- calendario CSS -->
+    <link rel="stylesheet" href="{{ asset('css/css_calendario.css')}}">
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -105,5 +108,6 @@
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="{{ asset('bootstrap/js/bootstrap.min.js')}}"></script>
+    <script src="{{ asset('js/js_calendario.js')}}"></script>
 </body>
 </html>
