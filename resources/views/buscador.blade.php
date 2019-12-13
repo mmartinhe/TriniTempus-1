cf8cf1610b4fbd9a13210318ac21a89b3a95cd52
MaZaJoM <jmartinzaballos@yahoo.com>
2019-12-13 12:58:44 +0100

    prueba layout

--- resources/views/buscador.blade.php
+++ resources/views/buscador.blade.php
@@ -1,108 +1,28 @@
-<!doctype html>
-<html lang="en">
+@extends('layouts.plantilla')
+
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <!-- Required meta tags -->
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>TriniTempus</title>
 
     <!--icono pestaña-->
     <link rel="ICON" type="iMAGEN/PNG" href="{{ asset('img/pestaña.png')}}"/>
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.min.css')}}">
+
+
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
-    <!-- Styles -->
-    <style>
-        html,
-        body {
-            background-color: #A0E5EB;
-            color: #636b6f;
-            font-family: 'Exo', sans-serif;
-            font-weight: 200;
-            height: 100vh;
-            margin: 0;
-        }
-        
-        .full-height {
-            height: 100vh;
-        }
-        
-        .flex-center {
-            align-items: center;
-            display: flex;
-            justify-content: center;
-        }
-        
-        .position-ref {
-            position: relative;
-        }
-        
-        .top-right {
-            position: absolute;
-            right: 10px;
-            top: 18px;
-        }
-        
-        .content {
-            text-align: center;
-        }
-        
-        .title {
-            font-size: 84px;
-        }
-        
-        .links> a {
-            color: #636b6f;
-            padding: 0 25px;
-            font-size: 13px;
-            font-weight: 600;
-            letter-spacing: .1rem;
-            text-decoration: none;
-            text-transform: uppercase;
-        }
-        
-        .m-b-md {
-            margin-bottom: 30px;
-        }
-        
-        /*---------------------------------------------------------------*/
 
     </style>
 </head>
 
 <body>
-    @if (Route::has('login'))
-    <div class="top-right links">
-        @auth
-        <a href="{{ url('/home') }}">Home</a>
-        <a href="{{ url('/buscador') }}">Buscador</a>@else
-        <a href="{{ route('login') }}">Logarse</a> @if (Route::has('register'))
-        <a href="{{ route('register') }}">Registrarse</a> @endif @endauth
-    </div>
-    @endif
-    <div class="content">
-        <div class="title m-b-md">
-            <a id="capa" href="{{ url('home')}}"><img  src="{{ asset('img/logo.png')}}" style='max-width: 250px'/></a>
-        </div>
-        <h1>VISTA BUSCADOR</h1>
-
-
-
-
-
-
-
-
-
-
-    </div>
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="{{ asset('bootstrap/js/bootstrap.min.js')}}"></script>
+    <h1>PRUEBA ARRIBA</h1>
+    
+    <h1>PRUEBA ABAJO</h1>
 </body>
 </html>
+
