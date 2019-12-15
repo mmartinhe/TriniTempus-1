b0dcdbca0b16b6e0d82734b224546146bf6858dc
LauraGuerrero <58034707+LauraGuerrero@users.noreply.github.com>
2019-12-15 19:35:06 +0100

    Modificaciones Home,Soporte y contacto

--- resources/views/contacto.blade.php
+++ resources/views/contacto.blade.php
@@ -1,82 +1,4 @@
-
-
-<!doctype html>
-<html lang="en">
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <title>TriniTempus</title>
-
-    <!--icono pestaña-->
-    <link rel="ICON" type="iMAGEN/PNG" href="{{ asset('img/pestaña.png')}}"/>
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.min.css')}}">
-
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-    <!-- Styles -->
-    <style>
-        html,
-        body {
-            background-color: #A0E5EB;
-            color: #636b6f;
-            font-family: 'Exo', sans-serif;
-            font-weight: 200;
-            height: 100vh;
-            margin: 0;
-        }
-        
-        .full-height {
-            height: 100vh;
-        }
-        
-        .flex-center {
-            align-items: center;
-            display: flex;
-            justify-content: center;
-        }
-        
-        .position-ref {
-            position: relative;
-        }
-        
-        .top-right {
-            position: absolute;
-            right: 10px;
-            top: 18px;
-        }
-        
-        .content {
-            text-align: center;
-        }
-        
-        .title {
-            font-size: 84px;
-        }
-        
-        .links> a {
-            color: #636b6f;
-            padding: 0 25px;
-            font-size: 13px;
-            font-weight: 600;
-            letter-spacing: .1rem;
-            text-decoration: none;
-            text-transform: uppercase;
-        }
-        
-        .m-b-md {
-            margin-bottom: 30px;
-        }
-        
-        /*---------------------------------------------------------------*/
-
-    </style>
-</head>
-
-<body>
+@extends('layouts.final')
 	@if (Route::has('login'))
 	<div class="top-right links">
 		@auth
@@ -86,6 +8,8 @@
 		<a href="{{ route('register') }}">Registrarse</a> @endif @endauth
 	</div>
 	@endif
+
+<!--contenido-->
 	<div class="content">
 		<div class="title m-b-md">
 			<a id="capa" href="{{ url('home')}}"><img  src="{{ asset('img/logo.png')}}" style='max-width: 250px'/></a>
@@ -102,18 +26,23 @@
 				</div>
 
 				<div class="col-sm-12 col-md-6 col-lg-4  offset-3">
-					<h1>Formulario de Contacto</h1>
-					<form action="{{url('/vistaFormularioContacto')}}" method="post" id="formulario">
-						<input type="hidden" name="_token" id="csrf-token" value="{{Session::token()}}">
-						<div id="datos_contacto" class="mt-2">
-							<p id="Email">Email:&nbsp&nbsp<input name="Email"></input>
-							</p>
-							<p id="asunto">Asunto:&nbsp&nbsp<input name="asunto"></input>
-							</p>
-							<p id="mensaje">Mensaje:&nbsp&nbsp<textarea name="mensaje"> </textarea></p>
-								<input type="submit" value="Enviar" id="btnEnviarFormularioContacto">
-						</div>
-					</form>
+					<h1>Redes sociales</h1>
+					<row>
+					<a href="" class="icon_social"><img  src="img/gorjeo.png" style="max-height: 50px" ></a>
+					<a href="" class="icon_social"><img  src="img/facebook.png" style="max-height: 50px"></a>
+					<a href="" class="icon_social"><img  src="img/google-plus.png" style="max-height: 50px"></a>
+					<a href="" class="icon_social"><img  src="img/youtube.png" style="max-height: 50px"></a>
+					<a href="" class="icon_social"><img  src="img/pinterest.png" style="max-height: 50px"></a>	
+					</row>
+					
+					<!--
+					<a href="" class="icon_social"><img  src="img/gorjeo.png"></a>
+					<a href="" class="icon_social"><img  src="img/facebook.png"></a>
+					<a href="" class="icon_social"><img  src="img/google-plus.png"></a>
+					<a href="" class="icon_social"><img  src="img/youtube.png"></a>
+					<a href="" class="icon_social"><img  src="img/pinterest.png"></a>	
+					-->
+												  
 				</div>
 			</div>
 		</div>
@@ -126,14 +55,7 @@
 
 
 
-	</div>
-	<!-- Optional JavaScript -->
-	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-	<script src="{{ asset('bootstrap/js/bootstrap.min.js')}}"></script>
-</body>
-</html>
+
 
 
 
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -25,7 +25,43 @@
         <a href="https://github.com/LauraGuerrero/TriniTempus">GitHub</a>
 
         <!-- prueba de hacer un scroll -->
-        <img class="img" src="{{asset('img/negocio2.jpg')}}">
+		<div class="col-12 flex-center">		
+			<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">		  
+			  <div class="carousel-inner">
+				<div class="carousel-item active">
+				  <img class="img" class="d-block w-60" src="img/negocio2.jpg" alt="First slide">
+				</div>
+				<div class="carousel-item">
+				  <img class="img" class="d-block w-60" src="img/negocio2.jpg" alt="Second slide">
+				</div>
+			  </div>
+			  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+				<span class="sr-only">Anterior</span>
+			  </a>
+			  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+				<span class="carousel-control-next-icon" aria-hidden="true"></span>
+				<span class="sr-only">Siguiente</span>
+			  </a>
+			</div>		
+		</div>
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+      <!--  <img class="img" src="{{asset('img/negocio2.jpg')}}"> -->
     </div>
 </div>
 <hr>
