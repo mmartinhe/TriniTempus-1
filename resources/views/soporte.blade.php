b3570d7eaef3702c1fe3fe81f62fcb93e38381ad
LauraGuerrero <58034707+LauraGuerrero@users.noreply.github.com>
2019-12-18 11:18:51 +0100

    SOPORTE

--- resources/views/soporte.blade.php
+++ resources/views/soporte.blade.php
@@ -40,7 +40,11 @@
 				<div class=" col-sm-12 col-md-6 col-lg-4 ">
 					<h1>Informacion de la Web</h1>
 					<p>
-						It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+						¡Bienvenid@ a TriniTempus!
+						<br>
+						Una web de compra-venta de servicios con una moneda de cambio distinta a la actualidad...
+						¡¡¡El tiempo!!!
+						Entra y podrás ofrecer tus propios servicios o solicitar los de otros con gran facilidad.
 
 
 					</p>
